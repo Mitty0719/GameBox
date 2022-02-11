8033c58bbc12f4c0a108dccc57155858f8afa04f
Mitty0719 <sprout071900@gmail.com>
2022-02-11 10:36:59 +0900

    Add playerBar move event

--- js/module/playerBar.js
+++ js/module/playerBar.js
@@ -4,15 +4,50 @@ export class PlayerBar{
         this.y = stageHeight - height - 20; // 20은 여분값
         this.width = width;
         this.height = height;
+        this.speed = 5;
 
         this.maxX = this.x + this.width;
         this.maxY = this.y + this.height;
+
+        this.stageWidth = stageWidth;
+        this.stageHeight = stageHeight;
+
+        window.addEventListener('keydown', this.moveStart.bind(this));
+        window.addEventListener('keyup', this.moveStop.bind(this));
     }
 
     draw(ctx){
+
         ctx.fillStyle = '#ffffff';
         ctx.beginPath();
         ctx.rect(this.x, this.y, this.width, this.height);
         ctx.fill();
+    }
+
+    moveStart(e){
+        console.log('hi');
+        if(e.keyCode === 37 && !this.isKeyDown){
+            this.keyInterval = setInterval(()=>{
+                if(this.x <= 0){
+                    return;
+                }
+                this.x -= this.speed;
+                this.maxX = this.x + this.width;
+            }, 10)
+            this.isKeyDown = true;
+        }else if(e.keyCode === 39 && !this.isKeyDown){
+            this.keyInterval = setInterval(()=>{
+                if(this.maxX >= this.stageWidth){
+                    return;
+                }
+                this.x += this.speed;
+                this.maxX = this.x + this.width;
+            }, 10)
+            this.isKeyDown = true;
+        }
+    }
+    moveStop(e){
+        clearInterval(this.keyInterval);
+        this.isKeyDown = false;
     }
 }
