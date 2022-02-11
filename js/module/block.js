6ab58240937d0d51184b7ad828324443a07b01eb
Mitty0719 <sprout071900@gmail.com>
2022-02-11 15:21:09 +0900

    Add addRow function when press spaceBar

--- js/module/block.js
+++ js/module/block.js
@@ -5,9 +5,12 @@ export class Block{
         this.index = index;
         this.width = (stageWidth / blockLength) - (this.gap * 2);
         this.height = 40;
-        
+        this.resize(stageWidth, blockLength, index);
+    }
+
+    resize(stageWidth, blockLength, index){
         this.x = (stageWidth / blockLength * index) + this.gap;
-        this.y = (this.gap * (rowIndex + 1)) + (this.height * rowIndex);
+        this.y = (this.gap * (this.rowIndex + 1)) + (this.height * this.rowIndex);
 
         this.maxX = this.x + this.width;
         this.maxY = this.y + this.height;
@@ -16,12 +19,14 @@ export class Block{
     draw(ctx){
         ctx.fillStyle = '#ffffff';
         ctx.beginPath();
+        ctx.fillText(`${this.rowIndex}-${this.index}`, this.x, this.y);
         ctx.rect(this.x, this.y, this.width, this.height);
         ctx.fill();
     }
 
     remove(blockGroup){
-        let removedBlocks = blockGroup.rows[this.rowIndex].blocks.filter(block => block !== this);
-        blockGroup.rows[this.rowIndex].blocks = removedBlocks;
+        // 삭제 안됨 -> 고치기
+        let removedBlocks = blockGroup.rows.filter(row => row.rowIndex === this.rowIndex).filter(block => block !== this);
+        blockGroup.rows[0].blocks = removedBlocks;
     }
 }
