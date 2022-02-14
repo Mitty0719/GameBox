39bd5ae287bde8d76ce3821cb320af52f57f30ab
Mitty0719 <sprout071900@gmail.com>
2022-02-14 12:59:35 +0900

    Dispose overLevel

--- js/game-1.js
+++ js/game-1.js
@@ -5,24 +5,24 @@ import { BlockGroup } from './module/blockGroup.js';
 import { level1, level2, level3, level4, level5 } from './module/level.js';
 export class Game1{
     constructor(){
-        this.level = level5;
+        this.levelNum = 1;
+        this.levelMax = 5;
+        this.isAdjust = false; // 레벨 조정 체크값
+        this.isStart = false; // 시작 텍스트 체크값
         this.canvas = document.createElement('canvas');
         this.ctx = this.canvas.getContext('2d');
         document.body.appendChild(this.canvas);
 
         this.resize();
 
-        this.ball = new Ball(this.stageWidth, this.stageHeight, 20, 10 * this.level.ballSpeedRatio);
-        this.playerBar = new PlayerBar(this.stageWidth, this.stageHeight, 300, 20);
-        this.blockGroup = new BlockGroup(this.stageWidth, this.stageHeight, this.level);
+        this.adjustLevel();
+        this.createElem();
 
         window.addEventListener('resize', this.resize.bind(this), false);
 
         requestAnimationFrame(this.animate.bind(this));
 
         setInterval(this.checkClear.bind(this), 1000);
-
-        console.log(this.blockGroup);
     }
 
     resize(){
@@ -39,7 +39,45 @@ export class Game1{
         this.ball.draw(this.ctx, this.stageWidth, this.stageHeight, this.playerBar, this.blockGroup);
         this.playerBar.draw(this.ctx);
         this.blockGroup.draw(this.ctx);
+
+        if(this.isAdjust){
+            let text = `Level${this.levelNum} Clear!`;
+
+            if(this.isStart){
+                text = `Level${this.levelNum+1} Start!`;
+            }
+            
+            this.showText(text);
+        }
+
         requestAnimationFrame(this.animate.bind(this));
+
+    }
+
+    createElem(){
+        let overLevel = this.levelNum > this.levelMax ? 0.2 * this.levelNum : 1;
+        this.ball = new Ball(this.stageWidth, this.stageHeight, 20, 10 * this.level.ballSpeedRatio * overLevel);
+        this.playerBar = new PlayerBar(this.stageWidth, this.stageHeight, 300, 20);
+        this.blockGroup = new BlockGroup(this.stageWidth, this.stageHeight, this.level);
+    }
+
+    adjustLevel(){
+        switch(this.levelNum){
+            case 1: this.level = level1; break;
+            case 2: this.level = level2; break;
+            case 3: this.level = level3; break;
+            case 4: this.level = level4; break;
+            case 5: this.level = level5; break;
+        }
+    }
+
+    showText(text){
+        console.log(text);
+        this.ctx.fillStyle = '#ffffff';
+        this.ctx.fillRect(0, 0, this.stageWidth, this.stageHeight);
+        this.ctx.font = '48px sesif';
+        this.ctx.fillStyle = '#000000';
+        this.ctx.fillText(text, this.stageWidth/2, this.stageHeight/2);
     }
 
     checkClear(){
@@ -48,8 +86,19 @@ export class Game1{
             blockCnt += row.blocks.length;
         });
 
-        if(blockCnt === 0){
-            console.log('clear');
+        if(blockCnt === 0 && !this.isAdjust){
+            this.isAdjust = true;
+            setTimeout(()=>{
+                this.isStart = true;
+            }, 2000)
+
+            setTimeout(()=>{
+                this.levelNum++;
+                this.adjustLevel();
+                this.createElem();
+                this.isAdjust = false;
+                this.isStart = false;
+            }, 4000)
         }
     }
 
